97a89d7a51a884c62e49b66951bb9f9d1a23051c
ldizon8 <lawdizon8@gmail.com>
2020-12-18 16:51:26 -0800

    Create simple timer

--- Breakdance App/Controller/HomeViewController.swift
+++ Breakdance App/Controller/HomeViewController.swift
@@ -1,7 +1,52 @@
+
 import UIKit
+import Foundation
+
+//
+//MARK: - HomeViewController
+//
 
 class HomeViewController: UIViewController{
+    //
+    //MARK: - HomeViewController Properties
+    //
+    
+    var timer: Timer?
+    @IBOutlet var timerLabel: UILabel!
+    
+    var timeLeft = 10 {
+        didSet {
+            timerLabel.text = String(timeLeft)
+        }
+    }
+    
+    //
+    //MARK: - HomeViewController View States
+    //
+    override func viewDidLoad() {
+        timerLabel.text = String(timeLeft)
+        
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        print("Timer fired!")
+    }
+    
+    //
+    //MARK: - Timer
+    //
+    
+    @objc func timerAction(){
+        
+        if timeLeft != 0 {
+            print(timeLeft)
+            timeLeft -= 1
+        }else{
+            timerLabel.text = "Timer Stopped!"
+            print("Timer Stopped!")
+            timer?.invalidate()
+        }
+    }
 
+    
     
 }
 
