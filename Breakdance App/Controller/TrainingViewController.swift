f533f8bce4b5617896e3ad15899af429f6ef030a
ldizon8 <lawdizon8@gmail.com>
2021-01-08 14:38:13 -0800

    Update file locations

---
+++ Breakdance App/Controller/TrainingViewController.swift
@@ -0,0 +1,9 @@
+//
+//  TrainingViewController.swift
+//  Breakdance App
+//
+//  Created by Lawrence Dizon on 1/8/21.
+//  Copyright © 2021 Lawrence Dizon. All rights reserved.
+//
+
+import Foundation
